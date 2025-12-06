1e63f5aed4ac67d458d93f86ef9430d724fa5391
Devon Nutting <36216700+DA-Nutt01@users.noreply.github.com>
2025-12-06 05:23:43 -0800

    Home.html example created for Web Dev WK 14

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # A route to the home page of the app
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
